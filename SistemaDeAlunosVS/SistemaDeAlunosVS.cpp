c69a6b58bed9e281863697620c3040cdb12a4012
Henryson <henrysonrios2007@gmail.com>
2025-05-09 17:44:50 +0100

    Correcoes e melhorias

--- SistemaDeAlunosVS/SistemaDeAlunosVS.cpp
+++ SistemaDeAlunosVS/SistemaDeAlunosVS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 
 //criacao da classe Aluno
 class Aluno {
@@ -21,31 +22,54 @@ public:
 //Criacao do vector para armazenar os objetos dos alunos
 std::vector<Aluno> listaAlunos;
 
-//criacao das funcoes
+float calcularMedia() {
+	std::cout << "\nAdicione quantas notas desejar (de 0 a 20), a media sera calculada automaticamente.\n"
+		<< "Quando ja tiver posto todas as notas, digite C para calcular a media do aluno.\n";
+
+	std::string inputDoUsuario = "";
+	int contadorDeNotas = 0;
+	float somaDasNotas = 0.0;
+	float media = 0.0;
+
+	while (true) {
+
+		std::cout << "Nota " << contadorDeNotas + 1 << ": ";
+		std::cin >> inputDoUsuario;
+		
+		if (inputDoUsuario == "C" || inputDoUsuario == "c") {
+			break;
+		}
+
+		try {
+			float nota = std::stof(inputDoUsuario);
+			
+			if (nota >= 0.0 && nota <= 20.0) {
+				somaDasNotas += nota;
+				++contadorDeNotas;
+			}
+			else {
+				std::cout << "\nPor favor, insira uma nota de 0 a 20.\n";
+			}
+		}
+		catch (...) {
+			std::cout << "\nEntrada invalida. Por favor digite um numero valido ou C para calcular.\n";
+		}
+	}
+	media = somaDasNotas / contadorDeNotas;
+	return media;
+}
+
 void adicionar() {
 
-	std::string nome;
-	int numNotas;
-	float media;
+	std::string nome = "";
+	float media = 0;
 
 	// Pegar do utilizador os atributos dos alunos
 	std::cout << "Nome: ";
 	std::getline(std::cin, nome);
 
-	std::cout << "Quantas notas deseja adicionar? ";
-	std::cin >> numNotas;
-
-	// Calcular a media das notas
-	float soma = 0;
-	for (int i = 1; i <= numNotas; i++) {
-		float nota;
-		std::cout << "Nota: ";
-		std::cin >> nota;
-		soma += nota;
-	}
-	media = soma / numNotas;
-
-	// Adicionar o nome e media do aluno a lista
+	//Chama função que calcula a media e atribui essa media à variavel
+	media = calcularMedia();
 
 	listaAlunos.emplace_back(nome, media);
 
@@ -100,20 +124,10 @@ void editar() {
 	while (it != listaAlunos.end()) {
 		if (it->nome == nome) {
 
-			int numNotas;
-			std::cout << "\n";
-			std::cout << "Quantas notas deseja por? ";
-			std::cin >> numNotas;
+			//Chama função que calcula a media e atribui essa media à variavel
+			float novaMedia = calcularMedia();
 
-			// Calcular a media das notas
-			float soma = 0;
-			for (int i = 1; i <= numNotas; i++) {
-				float nota;
-				std::cout << "Nota: ";
-				std::cin >> nota;
-				soma += nota;
-			}
-			it->media = soma / numNotas;
+			it->media = novaMedia;
 			std::cout << "\n";
 			std::cout << "Nova media: " << it->media << std::endl;
 			std::cout << "\n";
@@ -131,7 +145,11 @@ void editar() {
 
 
 int main() {
-	while (true) {
+	// imprimir apenas uma casa decimal 
+	std::cout << std::fixed << std::setprecision(2);
+
+	bool controleDeCiclo = true;
+	while (controleDeCiclo) {
 		int escolha;
 		std::cout << "Escolha uma das opcoes:" << std::endl;
 		std::cout << "1: Adicionar um aluno" << std::endl;
@@ -157,11 +175,13 @@ int main() {
 		case 4:
 			listar();
 			break;
-		case 5:
+		case 5: {
+			controleDeCiclo = false;
 			std::cout << "Programa encerrado.";
-			return 0;
+			break;
+		}
 		default:
-			std::cout << "\nOpcao invalida. Por favor, escolha 1, 2 ou 3.\n" << std::endl;
+			std::cout << "\nOpcao invalida. Por favor, escolha uma das opcoes disponiveis.\n" << std::endl;
 		}
 	}
 	return 0;
